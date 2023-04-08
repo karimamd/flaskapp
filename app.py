d5c2bbc2e2820355e4a7677f043758baf6a8acd9
Kareem Abdelsalam <kareem.abdelsalam@deliveroo.co.uk>
2023-04-08 14:31:49 +0100

    latest changes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 app.config['SECRET_KEY'] = "Kareem"
 
 db = SQLAlchemy(app)
-     
+
 class students(db.Model):
    id = db.Column('student_id', db.Integer, primary_key = True)
    name = db.Column(db.String(100))
